0152464efbdc376521447f2ce77981ed87a682ff
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2024-03-17 13:56:42 +0100

    Ethereum EVM

---
+++ Osnove Blockhain tehnologije/index1.js
@@ -0,0 +1,22 @@
+// Ethereum EVM
+// Ethereum je kao internet, ali umjesto da se koristi za razmjenu informacija,
+//  koristi se za razmjenu vrijednosti. To je platforma koja omogućava ljudima da izrade
+//  i koriste aplikacije koje rade na blockchain tehnologiji.
+// Ether (ETH) je kriptovaluta koja se koristi unutar Ethereum mreže. To je kao digitalni
+//  novac koji ljudi koriste za plaćanje transakcijskih naknada ili kao oblik vrijednosti za
+//  razmjenu unutar Ethereum ekosustava.
+
+// Ukratko, Ethereum je platforma koja omogućava izradu decentraliziranih aplikacija i
+// pametnih ugovora, koristeći blockchain tehnologiju.
+  
+
+// Transakcija validni prelaz iz jednog stanja u drugi/
+// Transakrije mozemo da pakujemo u blokove
+// Umjesto da se svaka transakcija obrađuje odvojeno, skupljaju se zajedno u blokove, kao 
+// slova na stranici. To čini rad mreže efikasnijim jer se više transakcija može obraditi
+//  istovremeno. 
+
+// Word state -mapira adresu na apstraktan acount state, ta adresa je kriptografski identifikator
+// Acount svaki ima svoju adresu , acount neki objekat
+// stanje sadrzi storidz, potencijalan kod i balans (integer - koliko ima etera),mogu da saljem sve dok su  velicine jednake
+// ili vece od nula adresi, ako imam veliki balans
